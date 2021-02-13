da4f364bd92092be2ce235aa87a50645200196fd
redrexaye <45308379+rizkibhaskara@users.noreply.github.com>
2021-02-13 16:55:49 +0700

    Update Invoice

---
+++ application/views/Home/invoice.php
@@ -0,0 +1,27 @@
+<div class="container">
+  
+    <div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">All Inovice</h6>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered table-striped" id="Table-Job-Order" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th class="text-center" scope="col">No Invoice</th>
+                        <th class="text-center" scope="col">Customer</th>
+                        <th class="text-center" scope="col">Tgl Invoice</th>
+                        <th class="text-center" scope="col">Grand Total</th>
+                        <th class="text-center" scope="col">Detail</th>
+                      
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+</div>
