a012a57af433c618633345eedb9e057e98e4cc1e
redrexaye <45308379+rizkibhaskara@users.noreply.github.com>
2021-02-16 23:08:11 +0700

    Fix Bug Table Laporan dan Data Kendaraan

--- application/views/Home/truck.php
+++ application/views/Home/truck.php
@@ -4,9 +4,11 @@
         <h1 class="h3 mb-0 text-gray-800">Seluruh Kendaraan</h1>
     </div> 
 
+    <div class="card shadow mb-4">
     <div class="card-header py-3">
         <h6 class="m-0 font-weight-bold text-primary">Kendaraan</h6>
     </div>
+
     <div class="card-body">
         <div class="table-responsive">
             <table class="table table-bordered table-striped" id="Table-Truck" width="100%" cellspacing="0">
