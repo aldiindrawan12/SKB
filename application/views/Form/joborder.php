45b2ec24f8d79946a9b6978ec3c76cbd54cd2b05
aldi indrawan <indrawanaldi75@gmail.com>
2021-02-23 14:45:55 +0700

    bahan untuk login

--- application/views/Form/joborder.php
+++ application/views/Form/joborder.php
@@ -21,7 +21,7 @@
                 <!-- form Job Order Baru -->
                 <form action="<?=base_url("index.php/form/insert_JO")?>" method="POST" class="row">
                     <div class="col-md-4 col-md-offset-4">
-                    <label class="form-label font-weight-bold " for="Customer">Customer</label>
+                        <label class="form-label font-weight-bold " for="Customer">Customer</label>
                         <select name="Customer" id="Customer" class="form-control selectpicker mb-4" data-live-search="true" required>
                             <?php if(count($customer_by_name)==0){?>
                                 <option class="font-w700" disabled="disabled" selected value="">Customer</option>
@@ -32,81 +32,62 @@
                                 <option value="<?=$value["customer_id"]?>"><?=$value["customer_name"]?></option>
                             <?php } ?>
                         </select>
-                    <label for="Muatan" class="form-label font-weight-bold ">Muatan</label>
-                    <input autocomplete="off" type="text" class="form-control mb-4" id="Muatan" name="Muatan" required>
-                </div>
-
-                <div class="col-md-4 col-md-offset-4">
-            <label class="form-label font-weight-bold" for="Supir">Supir</label>
-            <select name="Supir" id="Supir" class="form-control selectpicker" data-live-search="true" required>
-                <option class="font-w700 mb-4" disabled="disabled" selected value="">Supir Pengiriman</option>
-                <?php foreach($supir as $value){
-                    if($value["status_jalan"]!="Jalan"){?>
-                    <option value="<?=$value["supir_id"]?>"><?=$value["supir_name"]?></option>
-                <?php }
-                } ?>
-            </select>
-        </div>
-
-                <div class="col-md-4 col-md-offset-4">
-                    <label class="form-label font-weight-bold " for="Kendaraan">Kendaraan</label>
-                    <select name="Kendaraan" id="Kendaraan" class="form-control selectpicker" data-live-search="true" required>
-                        <option class="font-w700 font-weight-bold mb-4" disabled="disabled" selected value="">Kendaraan Pengiriman</option>
-                        <?php foreach($mobil as $value){
-                            if($value["status_jalan"]!="Jalan"){?>
-                            <option value="<?=$value["mobil_no"]?>"><?=$value["mobil_no"]."  ||  ".$value["mobil_max_load"]." Ton  ||  ".$value["mobil_jenis"]?></option>
-                        <?php }
-                        } ?>
-                    </select>
-                </div>
-
-                
-
-                
-
-                <div class="col-md-4 col-md-offset-4 mb-4">
-                    <label class="form-label font-weight-bold" for="Asal ">Asal</label>
-                    <input autocomplete="off" type="text" class="form-control" id="Asal" name="Asal" required>
-                </div>
-                <div class="col-md-4 col-md-offset-4 mb-4">
-                    <label class="form-label font-weight-bold" for="Tujuan">Tujuan</label>
-                    <input autocomplete="off" type="text" class="form-control" id="Tujuan" name="Tujuan" required>
-                </div>
-               
-
-                
-                <div class="col-md-4 col-md-offset-4 mb-4">
-                    <label for="Uang" class="form-label font-weight-bold">Uang Jalan</label>
-                    <input autocomplete="off" type="text" class="form-control" id="Uang" name="Uang" required onkeyup="terbilang()">
-                </div>
-                <div class="col-md-4 col-md-offset-4 mb-4">
-                    <label for="Terbilang" class="form-label font-weight-bold">Terbilang</label>
-                    <input autocomplete="off" type="text" class="form-control" id="Terbilang" name="Terbilang" required readonly>
-                </div>
-
-                <div class="col-md-4 col-md-offset-4">
-                    <label for="Keterangan" class="form-label font-weight-bold">Keterangan/Catatan</label>
-                    <textarea class="form-control" name="Keterangan" id="Keterangan" rows="3"></textarea>
-                </div>
-
-                <div class="col-md-4 col-md-offset-4 mt-5" >
-                <button type="submit" class="btn btn-success ml-3 mt-5 float-md-right">Simpan dan Cetak</button>
-                
-                <button type="reset" class="btn btn-outline-danger mb-3 mt-5  float-md-right">Reset</button>
-                    
-                   
-                    
-                </div>
-            </form>
+                    </div>
+                    <div class="col-md-4 col-md-offset-4">
+                        <label for="Muatan" class="form-label font-weight-bold ">Muatan</label> 
+                        <input autocomplete="off" type="text" class="form-control mb-4" id="Muatan" name="Muatan" required>
+                    </div>
+                    <div class="col-md-4 col-md-offset-4">
+                        <label class="form-label font-weight-bold" for="Supir">Supir</label>
+                        <select name="Supir" id="Supir" class="form-control selectpicker" data-live-search="true" required>
+                            <option class="font-w700 mb-4" disabled="disabled" selected value="">Supir Pengiriman</option>
+                            <?php foreach($supir as $value){
+                                if($value["status_jalan"]!="Jalan"){?>
+                                <option value="<?=$value["supir_id"]?>"><?=$value["supir_name"]?></option>
+                            <?php }
+                            } ?>
+                        </select>
+                    </div>
+                    <div class="col-md-4 col-md-offset-4">
+                        <label class="form-label font-weight-bold " for="Kendaraan">Kendaraan</label>
+                        <select name="Kendaraan" id="Kendaraan" class="form-control selectpicker" data-live-search="true" required>
+                            <option class="font-w700 font-weight-bold mb-4" disabled="disabled" selected value="">Kendaraan Pengiriman</option>
+                            <?php foreach($mobil as $value){
+                                if($value["status_jalan"]!="Jalan"){?>
+                                <option value="<?=$value["mobil_no"]?>"><?=$value["mobil_no"]."  ||  ".$value["mobil_max_load"]." Ton  ||  ".$value["mobil_jenis"]?></option>
+                            <?php }
+                            } ?>
+                        </select>
+                    </div>
+                    <div class="col-md-4 col-md-offset-4 mb-4">
+                        <label class="form-label font-weight-bold" for="Asal ">Asal</label>
+                        <input autocomplete="off" type="text" class="form-control" id="Asal" name="Asal" required>
+                    </div>
+                    <div class="col-md-4 col-md-offset-4 mb-4">
+                        <label class="form-label font-weight-bold" for="Tujuan">Tujuan</label>
+                        <input autocomplete="off" type="text" class="form-control" id="Tujuan" name="Tujuan" required>
+                    </div>
+                    <div class="col-md-4 col-md-offset-4 mb-4">
+                        <label for="Uang" class="form-label font-weight-bold">Uang Jalan</label>
+                        <input autocomplete="off" type="text" class="form-control" id="Uang" name="Uang" required onkeyup="terbilang()">
+                    </div>
+                    <div class="col-md-4 col-md-offset-4 mb-4">
+                        <label for="Terbilang" class="form-label font-weight-bold">Terbilang</label>
+                        <input autocomplete="off" type="text" class="form-control" id="Terbilang" name="Terbilang" required readonly>
+                    </div>
+                    <div class="col-md-4 col-md-offset-4">
+                        <label for="Keterangan" class="form-label font-weight-bold">Keterangan/Catatan</label>
+                        <textarea class="form-control" name="Keterangan" id="Keterangan" rows="3"></textarea>
+                    </div>
+                    <div class="col-md-12 col-md-offset-4 mt-5">
+                        <button type="submit" class="btn btn-success ml-3 mt-5 float-md-right">Simpan dan Cetak</button>
+                        <button type="reset" class="btn btn-outline-danger mb-3 mt-5  float-md-right">Reset</button>
+                    </div>
+                </form>
             <!-- end form Job Order Baru -->
-
-
-
-                                </div>
-                            </div>
-
-    
-</div>
+            </div>
+        </div>
+    </div>
 
 <!-- pop up add cutomer -->
 <div class="modal fade mt-5 px-5 py-5" id="popup-customer" tabindex="-1" role="dialog" aria-labelledby="modal-block-large" aria-hidden="true">
