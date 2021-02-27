b19efbb6628d8bcf6fbc87b604a74ee64715c4ab
redrexaye <45308379+rizkibhaskara@users.noreply.github.com>
2021-02-27 21:44:55 +0700

    update form bon fix

--- application/views/Form/form_bon.php
+++ application/views/Form/form_bon.php
@@ -8,7 +8,7 @@
             <!-- form transaksi bon -->
             <div class="container">
                 <form action="<?=base_url("index.php/form/insert_bon")?>" method="POST" class="row">
-                    <div class="col-md-5">
+                    <div class="col-md-4 col-md-offset-4 mb-4">
                         <label class="form-label" for="Supir_bon">Supir</label>
                         <select name="Supir_bon" id="Supir_bon" class="form-control selectpicker" data-live-search="true" required onchange="bon_user()">
                             <option class="font-w700" disabled="disabled" selected value="">Supir Pengiriman</option>
@@ -17,36 +17,42 @@
                             <?php } ?>
                         </select>
                        
-                        <label for="" class="form-label">Bon Hutang Saat Ini</label>
+                       
+                        
+                        
+                   
+                    </div>
+                    <div class="col-md-4 col-md-offset-4 mb-4">
+                    <label for="" class="form-label">Bon Hutang Saat Ini</label>
                         <input autocomplete="off" type="text" class="form-control" id="bon-saat-ini-tampilan" name="" disabled>
                         <input autocomplete="off" type="text" class="form-control" id="bon-saat-ini" name="" required hidden>
                         
-                        <label class="form-label" for="Jenis">Jenis Transaksi</label>
+                       
+                    </div>
+                    <div class="col-md-4 col-md-offset-4 mb-4">
+                    <label class="form-label" for="Jenis">Jenis Transaksi</label>
                         <select name="Jenis" id="Jenis" class="form-control custom-select" required onchange="nominal()">
                             <option class="font-w700" disabled="disabled" selected value="">Jenis Transaksi</option>
                             <option value="Pengajuan">Pengajuan</option>
                             <option value="Pembayaran">Pembayaran</option>
                         </select>
                         
-                        <label for="Nominal" class="form-label">Nominal</label>
-                        <input autocomplete="off" type="text" class="form-control" id="Nominal" name="Nominal" required onkeyup="nominal()">
-                   
+                       
                     </div>
 
                     <div class="col-md-4 col-md-offset-4 mb-4">
-                        <label class="form-label font-weight-bold" for="Asal ">Asal</label>
-                        <input autocomplete="off" type="text" class="form-control" id="Asal" name="Asal" required>
+                    <label for="Nominal" class="form-label">Nominal</label>
+                        <input autocomplete="off" type="text" class="form-control" id="Nominal" name="Nominal" required onkeyup="nominal()">
                     </div>
 
-
                      
-                    <div class="col-md-5">
+                    <div class="col-md-8 mb-4 ">
                         <label for="Keterangan" class="form-label">Keterangan/Catatan</label>
                         <textarea class="form-control" name="Keterangan" id="Keterangan" rows="3"></textarea>
                     </div>
-                    <div class="col-md-12 col-md-offset-4 mt-5  ">
-                       
-                    <button type="submit" class="btn btn-success mb-3 ml-3 mr-5 float-right">Simpan</button>    
+
+                    <div class="col-md-12 col-md-offset-4 mt-5 ">
+                    <button type="submit" class="btn btn-success mb-3 ml-3 float-right">Simpan</button>    
                     <button type="reset" class="btn btn-outline-danger mb-3 float-right">Reset</button> 
                         
                     </div>
--- application/views/login.php
+++ application/views/login.php
@@ -9,7 +9,8 @@
 
     <!-- Custom fonts for this template-->
     <link href="<?=base_url("assets/vendor/fontawesome-free/css/all.min.css")?>" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href=<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo base_url('assets/sweetalert2/sweetalert2.min.css') ?>">
 
     <!-- Custom styles for this template-->
@@ -17,15 +18,17 @@
 
 </head>
 
-<body class="bg-gradient-primary">
+<body class="bg-gradient-light">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-5">
+            <div class="col-md-5 mt-5 ">
                 <div class="card my-5">
                     <div class="card-body">
                         <div class="p-5">
                             <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Selamat Datang</h1>
+                            <img class="img-fluid mt-3 mb-3" src="<?php echo base_url('assets/img/logo.png')?> " alt="Gink Technology">
+                                <h1 class="h4 text-gray-900 mb-4 font-weight-bold">LOGIN</h1>
+                               
                             </div>
                             <form action="<?= base_url("index.php/login/login")?>" method="POST">
                                 <div class="form-group">
