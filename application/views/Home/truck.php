c3920be7e0f041eaf83742c0827c18d2ca9ccd8e
redrexaye <45308379+rizkibhaskara@users.noreply.github.com>
2021-03-08 15:31:10 +0700

    update tonase

--- application/views/Home/truck.php
+++ application/views/Home/truck.php
@@ -22,7 +22,7 @@
                     <tr>
                         <th class="text-center" scope="col">No Polisi</th>
                         <th class="text-center" scope="col">Jenis Kendaraan</th>
-                        <th class="text-center" scope="col">Maximum Load</th>
+                        <th class="text-center" scope="col">Maximum Load (Ton)</th>
                         <th class="text-center" scope="col">Aksi</th>
                     </tr>
                 </thead>
